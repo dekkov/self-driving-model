5492a0a6e3a84086531f351c329b82721e86c158
Hoang Tran <trhoang2003@gmail.com>
2024-10-05 20:04:31 -0700

    add road.js

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ const canvas=document.getElementById("myCanvas");
 canvas.width=200;
 
 const ctx = canvas.getContext("2d");
-const car=new Car(100,100,30,50);
+const car = new Car(100,100,30,50);
+const road = new Road(canvas.width/2,canvas.width);
 
 animate();
 
